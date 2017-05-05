4ce8f1cc7e03eb095a924a34f31eca00b7cbcacf
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2017-05-05 17:19:12 +0100

    Added billing alerts

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 import logging
 import asyncio
+import aiohttp
 import random
 from datetime import datetime
 
@@ -8,11 +9,12 @@ import boto3
 import botocore
 from prettytable import PrettyTable, MSWORD_FRIENDLY
 
-from opsdroid.matchers import match_apiai_action, match_crontab
+from opsdroid.matchers import match_apiai_action, match_crontab, match_regex
 from opsdroid.message import Message
 
 
 _LOGGER = logging.getLogger(__name__)
+CHAPI_ENDPOINT = "https://chapi.cloudhealthtech.com/olap_reports/"
 
 
 async def aws_get_client(service, config):
@@ -160,3 +162,41 @@ async def aws_stop_dev(opsdroid, config, message):
         _LOGGER.debug(response)
     else:
         await message.respond("I couldn't find any instances to shut down")
+
+async def get_aws_cost_for_period(api_key, period):
+    url = "{}cost/history?api_key={}&interval={}".format(CHAPI_ENDPOINT, api_key, period)
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                response = await resp.json()
+                return round(response['data'][-2][0][0], 2)
+
+@match_crontab("00 09 * * *")
+@match_regex("AWS bill yesterday")
+async def aws_billing_daily(opsdroid, config, message):
+    if not config.get("daily-billing-alerts", True):
+        return
+    if message is None:
+        connector = opsdroid.default_connector
+        message = Message("", None, connector.default_room, connector)
+    api_key = config.get("chapi-key", None)
+    if api_key is not None:
+        cost = await get_aws_cost_for_period(api_key, "daily")
+        await message.respond("Yesterday we spent £{:,} on AWS.".format(cost))
+    else:
+        await message.respond("I can't check the billing API without a key.")
+
+@match_crontab("00 09 01 * *")
+@match_regex("AWS bill last month")
+async def aws_billing_daily(opsdroid, config, message):
+    if not config.get("monthly-billing-alerts", True):
+        return
+    if message is None:
+        connector = opsdroid.default_connector
+        message = Message("", None, connector.default_room, connector)
+    api_key = config.get("chapi-key", None)
+    if api_key is not None:
+        cost = await get_aws_cost_for_period(api_key, "monthly")
+        await message.respond("Last month we spent £{:,} on AWS.".format(cost))
+    else:
+        await message.respond("I can't check the billing API without a key.")
